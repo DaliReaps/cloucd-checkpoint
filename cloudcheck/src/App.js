1a1f9005672ed304baed093796cf19c3d49d4d6d
Dali <chaouch.mohammed.ali@gmail.com>
2023-08-22 17:11:57 +0100

    first commit

---
+++ cloudcheck/src/App.js
@@ -0,0 +1,84 @@
+import logo from './logo.svg';
+import './App.css';
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import{useCallback,useState} from 'react'
+
+const containerStyle = {
+  width: '100%',
+  height: '720px',
+ 
+
+};
+
+const center = {
+  lat: 20.5937,
+  lng: 78.9629
+};
+function App() {
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: "AIzaSyDTuj-Mi-lrUY-ohHXYCIm6ynQGu0Hg-bA"  //need to define your google api key 
+  })
+
+const [map, setMap] = useState(null)
+
+  const onLoad = useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+    setMap(map)
+  }, [])
+
+  const onUnmount = useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <div style={{position:'relative'}}>
+    
+    {isLoaded ? <main class="py-6 px-4 sm:p-6 md:py-10 md:px-8">
+  <div class="max-w-4xl mx-auto grid grid-cols-1 lg:max-w-5xl lg:gap-x-20 lg:grid-cols-2">
+    <div class="relative p-3 col-start-1 row-start-1 flex flex-col-reverse rounded-lg bg-gradient-to-t from-black/75 via-black/0 sm:bg-none sm:row-start-2 sm:p-0 lg:row-start-1">
+      <h1 class="mt-1 text-lg font-semibold text-white sm:text-slate-900 md:text-2xl dark:sm:text-white">Beach House in China Town</h1>
+      
+    </div>
+    <div class="grid gap-4 col-start-1 col-end-3 row-start-1 sm:mb-6 sm:grid-cols-4 lg:gap-6 lg:col-start-2 lg:row-end-6 lg:row-span-6 lg:mb-0" >
+    <GoogleMap 
+    mapContainerStyle={containerStyle}
+    center={center}
+    zoom={7}
+    onLoad={onLoad}
+    onUnmount={onUnmount}
+  ></GoogleMap>
+    </div>
+    <dl class="mt-4 text-xs font-medium flex items-center row-start-2 sm:mt-1 sm:row-start-3 md:mt-2.5 lg:row-start-2">
+      
+      <dt class="sr-only">Location</dt>
+      <dd class="flex items-center">
+        <svg width="2" height="2" aria-hidden="true" fill="currentColor" class="mx-3 text-slate-300">
+          <circle cx="1" cy="1" r="1" />
+        </svg>
+        <svg width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mr-1 text-slate-400 dark:text-slate-500" aria-hidden="true">
+          <path d="M18 11.034C18 14.897 12 19 12 19s-6-4.103-6-7.966C6 7.655 8.819 5 12 5s6 2.655 6 6.034Z" />
+          <path d="M14 11a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z" />
+        </svg>
+        Collingwood, Ontario
+      </dd>
+    </dl>
+    <div class="mt-4 col-start-1 row-start-3 self-center sm:mt-0 sm:col-start-2 sm:row-start-2 sm:row-span-2 lg:mt-6 lg:col-start-1 lg:row-start-3 lg:row-end-4">
+      <button  >Check availability</button>
+    </div>
+    <p class="mt-4 text-sm leading-6 col-start-1 sm:col-span-2 lg:mt-6 lg:row-start-4 lg:col-span-1 dark:text-slate-400">
+      This sunny and spacious room is for those traveling light and looking for a comfy and cosy place to lay their head for a night or two. This beach house sits in a vibrant neighborhood littered with cafes, pubs, restaurants and supermarkets and is close to all the major attractions such as Edinburgh Castle and Arthur's Seat.
+    </p>
+  </div>
+</main>:<></>}
+    </div>
+    </div>
+  );
+}
+
+export default App;
